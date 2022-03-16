39ecc310e8b2ecf4501330f0485fea8eb366d70d
LoganRaisin <95636010+LoganRaisin@users.noreply.github.com>
2022-03-16 19:40:01 +0100

    constructeur + propperties de port.

--- TDNavire/ClasseMetier/Port.cs
+++ TDNavire/ClasseMetier/Port.cs
@@ -11,5 +11,100 @@
     /// </summary>
     internal class Port
     {
+        private string nom;
+        private string latitude;
+        private string longitude;
+        private int nbPortique;
+        private int nbQuaisPassager;
+        private int nbQuaisTanker;
+        private int nbQuaisSuperTanker;
+        private Dictionary<string, Navire> navireAttendus;
+        private Dictionary<string, Navire> navireArrives;
+        private Dictionary<string, Navire> navirePartis;
+        private Dictionary<string, Navire> navireEnAttente;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Port"/> class.
+        /// </summary>
+        /// <param name="nom">nom du port.</param>
+        /// <param name="latitude">latitude du port.</param>
+        /// <param name="longitude">longitude du port.</param>
+        /// <param name="nbPortique">nombre de portique pour accueillir cargo.</param>
+        /// <param name="nbQuaisPassager">nombre quais pour acueillir navire passagers.</param>
+        /// <param name="nbQuaisTanker">nombre quai pour tankers -130000tonnes.</param>
+        /// <param name="nbQuaisSuperTanker">quais tanker +130000tonnes.</param>
+        /// <param name="navireAttendus">string = id des navires attendus.</param>
+        /// <param name="navireArrives">navires presents dans le port.</param>
+        /// <param name="navirePartis">navires partis recemment.</param>
+        /// <param name="navireEnAttente">navires qui attendent un quai.</param>
+        public Port(string nom, string latitude, string longitude, int nbPortique, int nbQuaisPassager, int nbQuaisTanker, int nbQuaisSuperTanker, Dictionary<string, Navire> navireAttendus, Dictionary<string, Navire> navireArrives, Dictionary<string, Navire> navirePartis, Dictionary<string, Navire> navireEnAttente)
+        {
+            this.nom = nom;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.nbPortique = nbPortique;
+            this.nbQuaisPassager = nbQuaisPassager;
+            this.nbQuaisTanker = nbQuaisTanker;
+            this.nbQuaisSuperTanker = nbQuaisSuperTanker;
+            this.navireAttendus = navireAttendus;
+            this.navireArrives = navireArrives;
+            this.navirePartis = navirePartis;
+            this.navireEnAttente = navireEnAttente;
+        }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public string Nom { get => this.nom; set => this.nom = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public string Latitude { get => this.latitude; set => this.latitude = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public string Longitude { get => this.longitude; set => this.longitude = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public int NbPortique { get => this.nbPortique; set => this.nbPortique = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public int NbQuaisPassager { get => this.nbQuaisPassager; set => this.nbQuaisPassager = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public int NbQuaisTanker { get => this.nbQuaisTanker; set => this.nbQuaisTanker = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        public int NbQuaisSuperTanker { get => this.nbQuaisSuperTanker; set => this.nbQuaisSuperTanker = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        internal Dictionary<string, Navire> NavireAttendus { get => this.navireAttendus; set => this.navireAttendus = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        internal Dictionary<string, Navire> NavireArrives { get => this.navireArrives; set => this.navireArrives = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        internal Dictionary<string, Navire> NavirePartis { get => this.navirePartis; set => this.navirePartis = value; }
+
+        /// <summary>
+        /// Gets or sets.
+        /// </summary>
+        internal Dictionary<string, Navire> NavireEnAttente { get => this.navireEnAttente; set => this.navireEnAttente = value; }
     }
 }
